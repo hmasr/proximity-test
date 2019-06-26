16b16b61698ca38f7a6b7696f0bcff78344ee191
ninox92 <thomasreynders@hotmail.nl>
2019-06-26 22:25:11 +0200

    chore(logging): add logging

--- index.js
+++ index.js
@@ -1,33 +1,44 @@
 const Gpio = require("onoff").Gpio;
 const proximity = new Gpio(4, "out");
 
-var timer = startTimer();
+try {
+  var timer = startTimer();
 
-function restartTimer(timeout) {
-  clearTimeout(timeout);
-  timeout = null;
-  timeout = startTimer();
-}
-function startTimer() {
-  return setTimeout(() => {
-    console.log("Timeout, turn off monitor");
-    timer = null;
-  }, 10000);
-}
-
-proximity.watch((err, value) => {
-  if (err) {
-    throw err;
+  function restartTimer(timeout) {
+    console.log("restartTimer");
+    clearTimeout(timeout);
+    timeout = null;
+    timeout = startTimer();
+  }
+  function startTimer() {
+    console.log("startTimer");
+    return setTimeout(() => {
+      console.log("timer elapsed");
+      timer = null;
+    }, 10000);
   }
 
-  if (timer == null) {
-    console.log("Turn on monitor");
-  }
+  proximity.watch((err, value) => {
+    try {
+      if (err) {
+        throw err;
+      }
 
-  console.log(`Watch = ${value}`);
-  restartTimer(timer);
-});
+      if (timer == null) {
+        console.log("Turn on monitor");
+        timer = startTimer();
+      }
 
-process.on("SIGINT", () => {
-  proximity.unexport();
-});
+      console.log(`Watch = ${value}`);
+      restartTimer(timer);
+    } catch (error) {
+      console.error(error);
+    }
+  });
+
+  process.on("SIGINT", () => {
+    proximity.unexport();
+  });
+} catch (error) {
+  console.error(error);
+}
