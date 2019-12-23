cdaa9363efd1d1baa7887edde3de44e653a8e5ae
Thomas Reynders <thomasreynders@hotmail.nl>
2019-12-23 17:38:38 +0100

    fix(Gpio): Remove namespace declaration

--- src/gpio-proximity.ts
+++ src/gpio-proximity.ts
@@ -2,41 +2,39 @@ import { Gpio, BinaryValue } from 'onoff'
 import Timer from './timer'
 import { EventEmitter } from 'events'
 
-export namespace Proximity {
-  export class SignalProximity extends EventEmitter {
-    private readonly timer: Timer
-    private readonly gpio: Gpio
-    constructor(gpio: Gpio, timeout: number = 10000) {
-      super()
+export default class SignalProximity extends EventEmitter {
+  private readonly timer: Timer
+  private readonly gpio: Gpio
+  constructor(gpio: Gpio, timeout: number = 10000) {
+    super()
 
-      this.gpio = gpio
-      this.timer = new Timer(timeout)
-      this.timer.elapsed = function() {
-        console.log('timer elapsed')
+    this.gpio = gpio
+    this.timer = new Timer(timeout)
+    this.timer.elapsed = function() {
+      console.log('timer elapsed')
+    }
+    this.gpio.watch(this._onGpioWatch.bind(this))
+  }
+
+  private _onGpioWatch(err: Error | null | undefined, value: BinaryValue) {
+    try {
+      if (err) {
+        console.log(err)
+        throw err
       }
-      this.gpio.watch(this._onGpioWatch.bind(this))
-    }
+      console.log(`Watch = ${value}`)
 
-    private _onGpioWatch(err: Error | null | undefined, value: BinaryValue) {
-      try {
-        if (err) {
-          console.log(err)
-          throw err
-        }
-        console.log(`Watch = ${value}`)
-
-        this.timer.restart()
-      } catch (error) {
-        console.error(error)
-      }
+      this.timer.restart()
+    } catch (error) {
+      console.error(error)
     }
+  }
 
-    public start() {
-      this.timer.start()
-    }
+  public start() {
+    this.timer.start()
+  }
 
-    public stop() {
-      this.gpio.unexport()
-    }
+  public stop() {
+    this.gpio.unexport()
   }
 }
