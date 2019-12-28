29006f2224abce7fd22c45bb2bd0612e25214f2f
Thomas Reynders <thomasreynders@hotmail.nl>
2019-12-28 15:00:31 +0100

    chore(Program): Enable GPIO Proximity

--- src/index.ts
+++ src/index.ts
@@ -5,20 +5,17 @@ import BluetoothProximity from './bluetooth-proximity'
 export namespace Proximity {
   const bluetooth = new BluetoothProximity()
   bluetooth.on('add', peripheral => {
-    // console.log(peripheral)
-  })
-  bluetooth.once('ready', () => {
-    // console.log('BLE READY')
+    console.log(peripheral)
   })
 
   bluetooth.start()
 
-  // const gpio_4 = new Gpio(4, 'in', 'rising')
+  const gpio_4 = new Gpio(4, 'in', 'rising')
 
-  // const signalProximity = new SignalProximity(gpio_4)
-  // signalProximity.start()
+  const signalProximity = new SignalProximity(gpio_4)
+  signalProximity.start()
 
-  // process.on('SIGINT', () => {
-  //   signalProximity.stop()
-  // })
+  process.on('SIGINT', () => {
+    signalProximity.stop()
+  })
 }
