b5775c00865ba8f104ce35659feab480c4d191ab
ninox92 <thomasreynders@hotmail.nl>
2019-12-30 21:00:40 +0100

    feat(Program): Add vcgencmd to SignalProximity callbacks

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Gpio } from 'onoff'
 import SignalProximity from './gpio-proximity'
 import BluetoothProximity from './bluetooth-proximity'
-
+import { execSync } from 'child_process'
 export namespace Proximity {
   const bluetooth = new BluetoothProximity()
   bluetooth.on('add', peripheral => {
@@ -15,9 +15,19 @@ export namespace Proximity {
   const signalProximity = new SignalProximity(gpio4)
   signalProximity.on('begin', function() {
     console.log('Turn display ON')
+    try {
+      execSync('vcgencmd display_power 1')
+    } catch (error) {
+      console.error(error)
+    }
   })
   signalProximity.on('end', function() {
     console.log('Turn display OFF')
+    try {
+      execSync('vcgencmd display_power 0')
+    } catch (error) {
+      console.error(error)
+    }
   })
 
   process.on('SIGINT', () => {
