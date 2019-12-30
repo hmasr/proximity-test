4a9d3cd19ce024d3310d8867f124575d7747b276
ninox92 <thomasreynders@hotmail.nl>
2019-12-30 20:35:03 +0100

    feat(Gpio): Add replace console.log with debug

--- src/gpio-proximity.ts
+++ src/gpio-proximity.ts
@@ -1,17 +1,21 @@
 import { Gpio, BinaryValue } from 'onoff'
 import Timer from './timer'
 import { EventEmitter } from 'events'
+import Debug from 'debug'
+
+const debug = Debug('signal')
 
 export default class SignalProximity extends EventEmitter {
   private readonly timer: Timer
   private readonly gpio: Gpio
+
   constructor(gpio: Gpio, timeout: number = 10000) {
     super()
 
     this.gpio = gpio
     this.timer = new Timer(timeout)
     this.timer.elapsed = function() {
-      console.log('timer elapsed')
+      debug('timer elapsed')
     }
     this._onGpioWatch(null, this.gpio.readSync())
     this.gpio.watch(this._onGpioWatch.bind(this))
@@ -23,14 +27,17 @@ export default class SignalProximity extends EventEmitter {
       return
     }
 
+    debug(`GPIO=${value}`)
     this.timer.restart()
   }
 
   public start() {
+    debug('start')
     this.timer.start()
   }
 
   public stop() {
+    debug('stop')
     this.timer.stop()
     this.gpio.unexport()
   }
