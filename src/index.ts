07b90f8fb1b03dc65e8202debd312a26869f3842
ninox92 <thomasreynders@hotmail.nl>
2019-12-31 13:54:26 +0100

    chore(Program): Move index.ts to example and export Proximity

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,4 @@
-import { Gpio } from 'onoff'
-import SignalProximity from './gpio-proximity'
-import BluetoothProximity from './bluetooth-proximity'
-import { execSync } from 'child_process'
-import { config } from 'dotenv'
+import SignalProximity from '../src/gpio-proximity'
+import BluetoothProximity from '../src/bluetooth-proximity'
 
-export namespace Proximity {
-  // Load dotenv config file
-  config()
-
-  const bluetooth = new BluetoothProximity()
-  bluetooth.on('add', peripheral => {
-    console.log(peripheral)
-  })
-
-  bluetooth.start()
-
-  const gpio4 = new Gpio(4, 'in', 'both')
-
-  const signalProximity = new SignalProximity(
-    gpio4,
-    process.env.SIGNAL_TIMEOUT as number | undefined
-  )
-  signalProximity.on('begin', function() {
-    console.log('Turn display ON')
-    try {
-      execSync('vcgencmd display_power 1')
-    } catch (error) {
-      console.error(error)
-    }
-  })
-  signalProximity.on('end', function() {
-    console.log('Turn display OFF')
-    try {
-      execSync('vcgencmd display_power 0')
-    } catch (error) {
-      console.error(error)
-    }
-  })
-
-  process.on('SIGINT', () => {
-    signalProximity.stop()
-  })
-}
+export { SignalProximity, BluetoothProximity }
